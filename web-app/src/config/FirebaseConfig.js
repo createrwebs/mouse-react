bc54f9f7c0b283cb1a37384a737421c8c279922c
websbringer <websbringer@gmail.com>
2024-04-13 16:45:50 +0700

    add

--- web-app/src/config/FirebaseConfig.js
+++ web-app/src/config/FirebaseConfig.js
@@ -1,10 +1,10 @@
 export const FirebaseConfig = {
 	"projectId": "fcmthaimove",
-	"appId": "1:619154429073:web:de3ee8b2ea9f1f375e5b30",
+	"appId": "1:619154429073:web:a345cc74c6d619f45e5b30",
 	"databaseURL": "https://fcmthaimove.firebaseio.com",
 	"storageBucket": "fcmthaimove.appspot.com",
 	"locationId": "us-central",
-	"apiKey": "AIzaSyCauJQdItKzxV4ghKrdOPd6hkY5fgoTBig",
+	"apiKey": "AIzaSyC5bSufdm8wXzZsMSasYQhwJ5umMXgglZU",
 	"authDomain": "fcmthaimove.firebaseapp.com",
 	"messagingSenderId": "619154429073"
 };
